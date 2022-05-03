fd764ac8e335ea090970fa5458714b00b04aafd9
Yannik Sander <yannik@floxdev.com>
2022-05-03 12:41:14 +0200

    Use new capacitor interface

--- flake.nix
+++ flake.nix
@@ -5,41 +5,39 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.capacitor.url = "github:flox/capacitor";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, capacitor, flake-utils }:
+  outputs = { self, nixpkgs, capacitor, flake-utils,... } @ args:
+
     let
       version = builtins.substring 0 8 self.lastModifiedDate;
       flake = flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in
-      with pkgs;
-      rec {
-        packages = flake-utils.lib.flattenTree {
-          hello-go = buildGoModule rec {
-            pname = "hello-go";
-            inherit version;
-            # In 'nix develop', we don't need a copy of the source tree
-            # in the Nix store.
-            src = ./.;
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in
+        with pkgs;
+        rec {
+          packages = flake-utils.lib.flattenTree {
+            hello-go = buildGoModule rec {
+              pname = "hello-go";
+              inherit version;
+              # In 'nix develop', we don't need a copy of the source tree
+              # in the Nix store.
+              src = ./.;
 
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake sha256 and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remeber to bump this hash when your dependencies change.
-            #vendorSha256 = pkgs.lib.fakeSha256;
+              # This hash locks the dependencies of this package. It is
+              # necessary because of how Go requires network access to resolve
+              # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
+              # details. Normally one can build with a fake sha256 and rely on native Go
+              # mechanisms to tell you what the hash should be or determine what
+              # it should be "out-of-band" with other tooling (eg. gomod2nix).
+              # To begin with it is recommended to set this, but one must
+              # remeber to bump this hash when your dependencies change.
+              #vendorSha256 = pkgs.lib.fakeSha256;
 
-            vendorSha256 = "sha256-hnLItaDj5nVedbkpbPUw/A/7Iu2q1VwlYWzhvshWThk=";
+              vendorSha256 = "sha256-hnLItaDj5nVedbkpbPUw/A/7Iu2q1VwlYWzhvshWThk=";
+            };
           };
-        };
-        defaultPackage = packages.hello-go;
-      }
+          defaultPackage = packages.hello-go;
+        }
       );
-      capacitor-apps = capacitor.lib.makeApps combined nixpkgs;
-
-      combined = nixpkgs.lib.recursiveUpdate flake capacitor-apps;
     in
-    combined;
+    capacitor.lib.capacitate args inputs ( _: flake);
 }
